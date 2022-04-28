9d4061f4314aa78176e1d5f4a2bb856e75cf44f4
iw-Shawn <39892856+iw-Shawn-Chiu@users.noreply.github.com>
2022-04-28 17:27:55 +0800

    canvas

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <FabricDemo msg="Fabric Demo Page"/>
+  <div>
+    <canvas id="canvas" width="300" height="200"></canvas>
+  </div>
 </template>
 
 <script>
 import FabricDemo from './components/FabricDemo.vue'
+import fabric from 'fabric'
 
 export default {
   name: 'App',
   components: {
     FabricDemo
+  },
+  mounted() {
+    var canvas = new fabric.Canvas('canvas');
+    var rect = new fabric.Rect({
+      top: 50,
+      left: 100,
+      width: 100,
+      height: 70,
+      fill: 'red'
+    });
+    canvas.add(rect);
   }
 }
 </script>
@@ -22,5 +36,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+canvas {
+  border:  1px dashed black;
 }
 </style>
