f6fd96ef619d67fc67fbad3ff877deb5c4c4e4cf
iw-Shawn <39892856+iw-Shawn-Chiu@users.noreply.github.com>
2022-04-29 17:14:49 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,15 @@ module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
     ? '/fabric_demo/'
     : '/',
+  configureWebpack: {
+    plugins : [
+      new webpack.optimize.UglifyJsPlugin({ 
+        uglifyOptions: { 
+          compress: false, 
+          warnings: false, 
+          mangle: false
+        }
+      }),
+    ]
+  }
 })
