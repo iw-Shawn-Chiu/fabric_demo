ec9c0eeacb51adce358d08303608e10b6d6e2f13
iw-Shawn <39892856+iw-Shawn-Chiu@users.noreply.github.com>
2022-04-29 17:32:00 +0800

    discard

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
-import { createApp, use } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue';
-
-/* 註冊所有組件為全域 */
-use(BootstrapVue)
 
 createApp(App).mount('#app')
