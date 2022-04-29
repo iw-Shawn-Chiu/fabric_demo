ee0945432747af0820fe4e012b6c255cd9063d53
iw-Shawn <39892856+iw-Shawn-Chiu@users.noreply.github.com>
2022-04-29 17:02:42 +0800

    bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+import Vue from 'vue';
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue';
 
-createApp(App).mount('#app')
+/* 註冊所有組件為全域 */
+Vue.use(BootstrapVue);
+
+// createApp(App).mount('#app')
+Vue.createApp(App).mount('#app');
